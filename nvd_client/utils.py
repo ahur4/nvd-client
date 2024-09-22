daec713b6ab1af8411a3ed5c5ac58c3c83f79c3c
ahur4 <95452522+ahur4@users.noreply.github.com>
2024-09-22 16:27:00 +0330

    change input date types to datetime datatype instead of str

--- nvd_client/utils.py
+++ nvd_client/utils.py
@@ -1,36 +1,39 @@
 import re
 from datetime import datetime
-from .exceptions import InvalidDateFormatError, InvalidParametersError, InvalidCVEIDError
 from typing import Optional, List
 
+import pytz
 
-def validate_date_format(date_str):
-    # Define the regex pattern for YYYY-MM-DD format
-    pattern = r'^\d{4}-\d{2}-\d{2}$'
+from nvd_client.exceptions import InvalidDateFormatError, InvalidParametersError, InvalidCVEIDError
+
+# Use Iran's timezone
+IRAN_TZ = pytz.timezone('Asia/Tehran')
+
+
+def validate_date_format(date_str: str):
+    # Define the regex pattern for ISO 8601 format with timezone
+    pattern = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$'
     # Check if the date_str matches the pattern
     if re.match(pattern, date_str):
         try:
-            # Try to parse the date to ensure it's a valid date
-            datetime.strptime(date_str, '%Y-%m-%d')
+            # Parse the date to ensure it's a valid date
+            datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
             return True
         except ValueError:
             return False
-    else:
-        return False
+    return False
 
 
-def convert_date_to_iso(date_str):
-    if validate_date_format(date_str):
-        # Parse the input date
-        date = datetime.strptime(date_str, '%Y-%m-%d')
-        # Convert to the desired format
-        iso_format_date = date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+def convert_date_to_iso(date: datetime):
+    if isinstance(date, datetime):
+        # Convert to the desired ISO format without timezone information
+        iso_format_date = date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]  # Truncate to milliseconds
         return iso_format_date
     else:
-        raise InvalidDateFormatError(date_str)
+        raise InvalidDateFormatError(f"Invalid datetime object: {date}")
 
 
-def validate_and_convert_dates(start_date: Optional[str], end_date: Optional[str], param_names: List[str]):
+def validate_and_convert_dates(start_date: Optional[datetime], end_date: Optional[datetime], param_names: List[str]):
     if start_date and end_date:
         return convert_date_to_iso(start_date), convert_date_to_iso(end_date)
     elif start_date or end_date:
